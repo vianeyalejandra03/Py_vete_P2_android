9b6252dd9e73a83fa8428718737148e56ef0607e
FlutLab.io <>
2021-05-10 19:39:14 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -79,14 +79,13 @@ class Inicio extends StatelessWidget {
         child: Column(
           children: <Widget>[
             //hacemos la primera fila de el mapa
-
             Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
               Column(
                 children: <Widget>[
                   Padding(
                     padding: EdgeInsets.all(10),
                     child: RaisedButton(
-                      color: Colors.grey,
+                      color: Colors.white,
                       shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
 
                       onPressed: () {
@@ -106,9 +105,31 @@ class Inicio extends StatelessWidget {
                     ), //boton inicio
                   ) //fin padding
                 ], //fin widget niño
-              ), //fin de column dentro de column
-            ]), //cierre de row- fila 1
-          ], //cierre de children widget
+              ), //fin de column dentro de column interna 1
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: RaisedButton(
+                      color: Colors.white,
+                      shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/empresa");
+                      },
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: Center(
+                          child: Text("EMPRESA", textAlign: TextAlign.center),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ), //r1 columna 2
+            ] //segundo children fin 2 widget
+                ), //cierre de row- fila 1
+          ], //cierre de children widget 1
         ), //cierre de column en el child
       ), //fin del container
     ); //fin scaffold
