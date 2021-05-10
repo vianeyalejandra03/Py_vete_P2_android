c261e464211714eafc05f44352c4bda4b2aac76f
FlutLab.io <>
2021-05-10 19:45:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -129,6 +129,55 @@ class Inicio extends StatelessWidget {
               ), //r1 columna 2
             ] //segundo children fin 2 widget
                 ), //cierre de row- fila 1
+
+//inicio de la fila 2
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/productos");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text("PRODUCTOS", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ), // row 2 fin columna 1
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        color: Colors.white,
+                        shape: new RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/contacto");
+                        },
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Text("CONTACTO", textAlign: TextAlign.center),
+                          ),
+                        ),
+                      ),
+                    )
+                  ], //fin children del 2-2
+                ), //fin row2 columna 2
+              ],
+            ), //fin row 2 fila 2
           ], //cierre de children widget 1
         ), //cierre de column en el child
       ), //fin del container
